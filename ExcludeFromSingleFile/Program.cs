c99c39c1842ae1ba62bd90da0b7e86fec9c66d80
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-04-04 09:45:15 +0900

    プロジェクト ファイルを追加します。

---
+++ ExcludeFromSingleFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ExcludeFromSingleFile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using var processModule = Process.GetCurrentProcess().MainModule;
+			Console.WriteLine(File.ReadAllText(Path.Combine(Path.GetDirectoryName(processModule.FileName), "TextFile1.txt")));
+			Console.ReadKey();
+		}
+	}
+}
